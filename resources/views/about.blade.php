88b59671333e953494e875710f99aa670059faad
Aditya27T <aditpoetra25@gmail.com>
2022-02-21 09:23:05 +0700

    modul 5

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,5 +4,5 @@
   <h1> Halaman About </h1>
   <h3> {{ $nama }} </h3>
   <p> {{ $email }} </p>
-  <img src="image/bpcl.jfif" alt="tuturu panjaitan" width="500px">
+  <img src="image/cheese-dance.gif" alt="tuturu panjaitan" width="500px">
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,8 @@ use App\Http\Controllers\ContactController;
 
 
 Route::get('/', function () {
-    return view('home', [
-        "title" => "Home"
+    return view('index', [
+        "title" => "Beranda"
     ]);
 });    
 
@@ -26,7 +26,7 @@ Route::get('/about', function () {
         "title" => "About",
         "nama" => "Aditya Eka R",
         "email" => "aditpoetra25@gmail.com",
-        "gambar" => "bpcl.jfif"
+        "gambar" => "adittt.jfif"
     ]);
 });
 
@@ -37,3 +37,11 @@ Route::get('/gallery', function () {
 });
 
 Route::resource('/contacts', ContactController::class);
+
+Auth::routes();
+
+
+Route::group(['middleware' => ['auth']], function (){
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+});
+
